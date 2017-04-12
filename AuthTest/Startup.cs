a1fe29f4719589bc2c2486a1852053dce1e58afb
qinyikai <yikai.qin@talebase.com>
2017-04-12 16:15:49 +0800

    添加项目文件。

---
+++ AuthTest/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(AuthTest.Startup))]
+
+namespace AuthTest
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+        }
+    }
+}
